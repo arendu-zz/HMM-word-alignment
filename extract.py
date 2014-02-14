d8e7feecdd94654e7689e3a03a8fce94fde97994
Aric Velbel <aric@localhost.localdomain>
2014-02-14 13:12:24 -0500

    minor pipeline tweaks to extract and merge

--- extract.py
+++ extract.py
@@ -9,7 +9,7 @@ l = [[int(n) for n in e.split()] for e in f]
 m = [[s,e,f] for [s,e,f] in l if s <= 1000] # leq because of first line fencepost issue
 z = [[(u-1, v-1) for [s, u, v] in m if s == i] for i in range(1000)]
 for l in z[1:]:
-    print " ".join(["%d-%d" % (u, v) for (u, v) in l])
+    print " ".join(["%d-%d" % (v, u) for (u, v) in l]) # be careful about which is which
 
 
 
