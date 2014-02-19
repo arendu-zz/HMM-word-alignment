51ce5790f52aa5b832c949d6780568696caf5e58
Aric Velbel <aric@localhost.localdomain>
2014-02-18 20:03:34 -0500

    extract uses fileinput; flipped u and v for a special case

--- extract.py
+++ extract.py
@@ -3,13 +3,19 @@ convert Adi's model1 etc format to Adam + Matt's format
 right now it just reads the alignments from a file called 'u'; TODO proper fileinput
 """
 
-fn = "u"
-f = open(fn).read().splitlines()
+import fileinput
+
+#fn = "u"
+#f = open(fn).read().splitlines()
+f = []
+for line in fileinput.input():
+    f.append(line)
 l = [[int(n) for n in e.split()] for e in f]
 m = [[s,e,f] for [s,e,f] in l if s <= 1000] # leq because of first line fencepost issue
 z = [[(u-1, v-1) for [s, u, v] in m if s == i] for i in range(1000)]
 for l in z[1:]:
-    print " ".join(["%d-%d" % (v, u) for (u, v) in l]) # be careful about which is which
+    print " ".join(["%d-%d" % (u, v) for (u, v) in l]) # be careful about which is which
+    # BACKWARDS RIGHT NOW!
 
 
 
